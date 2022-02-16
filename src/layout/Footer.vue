0facb7197183221b6c9efc3b2c4d0ec06b1b1237
黄炜业 <huangwy@sinolvc.com>
2022-02-16 15:01:56 +0800

    初次提交

---
+++ src/layout/Footer.vue
@@ -0,0 +1,15 @@
+<template>
+  <a-layout-footer :style="{ textAlign: 'center', letterSpacing: '1px' }">
+    物流管理系统 ©2022 Created by 广东白云学院 - wjc
+  </a-layout-footer>
+</template>
+
+<script>
+export default {
+name: "Footer"
+}
+</script>
+
+<style scoped>
+
+</style>
